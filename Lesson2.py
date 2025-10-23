b21d402876e1d4661e9a29e1fdb3009657395c13
baranovYauheni <70491907+baranovYauheni@users.noreply.github.com>
2025-10-23 22:56:08 +0400

    add file

--- Lesson2.py
+++ Lesson2.py
@@ -5,12 +5,13 @@ def filter_even(numbers):
     evens = [n for n in numbers if n % 2 == 0]
     return  evens
 
+numbers = [1, 2, 3, 4, 5, 6]
+
 def calculate_stats(numbers):
     total = st.median(numbers) + st.mean(numbers)
     return st.mean(numbers), st.median(numbers), total
 
 kort = calculate_stats(numbers)
-print(kort)
 
 def greet_users(*names):
     for i in names:
